db9584ee0ba8681348647a13e77f6d178c801417
aheff20 <aidan.m.heffron@gmail.com>
2022-07-08 09:06:28 -0500

    compile done?
    hopefully

--- value.cs
+++ value.cs
@@ -242,16 +242,45 @@ namespace numbers
             }
         }
 
-        public static value make_value__5(string s)
+        public static value make_value__6(string s)
         {
-            if (s.Contains("\""))
+            try
+            {
+                if (s.Contains("."))
+                {
+                    throw new Exception();
+                }
+                numbers.value ans = new numbers.value();
+                ans.V = Convert.ToInt32(s);
+                ans.Kind = Value_Kind.Number_Kind;
+                return ans;
+            }
+            catch(Exception e){}
+
+            try
+            {
+                numbers.value ans = new numbers.value();
+                ans.V = Convert.ToDouble(s);
+                ans.Kind = Value_Kind.Number_Kind;
+                return ans;
+            }
+            catch (Exception e) { }
+
+            try
             {
                 numbers.value ans = new numbers.value();
                 ans.S = s;
                 ans.Kind = Value_Kind.String_Kind;
                 return ans;
             }
-            else if(s.Contains(".")){
+            catch (Exception e) { }
+
+            throw new Exception("Bad string");
+        }
+
+        public static value make_value__5(string s)
+        {
+            if(s.Contains(".")){
                 numbers.value ans = new numbers.value();
                 ans.V = Convert.ToDouble(s);
                 ans.Kind = Value_Kind.Number_Kind;
