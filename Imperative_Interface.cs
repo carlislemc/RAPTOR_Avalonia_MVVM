f39d25b7106a7756125d5ea500dff654afcccd68
Martin Carlisle <martin.c.carlisle@gmail.com>
2022-07-19 14:08:33 -0500

    Fixing circular dependence

---
+++ Imperative_Interface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeGenerators
+{
+    public interface Imperative_Interface : raptor.Generate_Interface
+    {
+        void Done_Method();
+        void Declare_Procedure(
+            string name,
+            string[] args,
+            bool[] arg_is_input,
+            bool[] arg_is_output);
+    }
+}
