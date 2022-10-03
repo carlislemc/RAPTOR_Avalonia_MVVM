692e005ff0265cb034e23367dd2260f414a3a10d
carlislem <carlislem@tamu.edu>
2022-10-03 15:41:57 -0500

    Updating install script with Linux zip and instructions for Mac

--- wix_compile_installer.bat
+++ wix_compile_installer.bat
@@ -1,2 +1,6 @@
 "C:\Program Files (x86)\WixEdit\wix-3.0.5419.0\candle.exe" -nologo "C:\Users\carlislem\source\repos\RAPTOR_Avalonia_MVVM\wixedit_raptor.wxs" -out "C:\Users\carlislem\source\repos\RAPTOR_Avalonia_MVVM\wixedit_raptor.wixobj"  -ext WixUtilExtension  -ext WixUIExtension  -ext WixNetFxExtension
 "C:\Program Files (x86)\WixEdit\wix-3.0.5419.0\light.exe" -nologo "C:\Users\carlislem\source\repos\RAPTOR_Avalonia_MVVM\wixedit_raptor.wixobj" -out "C:\Users\carlislem\source\repos\RAPTOR_Avalonia_MVVM\wixedit_raptor.msi"  -ext WixUtilExtension  -ext WixUIExtension  -ext WixNetFxExtension
+cd bin\release
+zip raptor_avalonia.zip -r publish
+cd ..\..
+echo "On Hulk, update the Payload folder in raptor and run updatepayload.sh"
