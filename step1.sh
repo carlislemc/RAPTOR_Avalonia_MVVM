6ac8b1deb29a9f4499369a622fef37210981e571
carlislem <carlislem@tamu.edu>
2023-04-14 11:22:07 -0500

    Add Mac instructions

---
+++ step1.sh
@@ -0,0 +1,4 @@
+﻿rm -R publish.old
+mv publish publish.old
+unzip raptor_avalonia.zip
+cp -v -r -u publish/* Payload/raptor.app/Contents/MacOS
---
+++ updatepayload.sh
@@ -0,0 +1,5 @@
+﻿( cd Payload && find . | cpio -o --format odc --owner 0:80 | gzip -c ) > flat/Payload
+mkbom -u 0 -g 80 ~/raptor/Payload/ flat/Bom
+du -BK ~/raptor/Payload
+echo "Check this value in PackageInfo"
+( cd flat && xar --compression none -cf "../raptor_avalonia.pkg" * )
